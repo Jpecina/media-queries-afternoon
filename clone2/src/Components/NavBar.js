9dc90d4071035850d4585f06e0a819ad49c243d9
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-02-19 15:56:26 -0600

    first-committ

---
+++ clone2/src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavBar(){
+    return (
+        <div>
+            <div className = "nav-bar">
+                <ul className="nav-item">SERVICES</ul>
+                <ul className="nav-item">PORTFOLIO</ul>
+                <ul className="nav-item">ABOUT</ul>
+                <ul className="nav-item">TEAM</ul>
+                <ul className="nav-item">CONTACT</ul>
+            </div>
+            <div className="nav-large">
+            
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
